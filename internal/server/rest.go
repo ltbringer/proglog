de554642560bb8ae6c060b23145fb2244657aa75
ltbringer <amresh.venugopal@gmail.com>
2023-07-07 00:52:57 +0100

    doc: rest.go

--- internal/server/rest.go
+++ internal/server/rest.go
@@ -7,33 +7,46 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// LogStore represents a store for logs.
 type LogStore struct {
 	Log *Log
 }
 
+// ProduceRequest represents a request to produce a new record.
 type ProduceRequest struct {
 	Record Record `json:"record"`
 }
 
+// ProduceResponse represents a response after producing a record.
 type ProduceResponse struct {
 	Offset Offset `json:"offset"`
 }
 
+// ConsumeRequest represents a request to consume a record.
 type ConsumeRequest struct {
 	Offset Offset `json:"offset"`
 }
 
+// ConsumeResponse represents a response after consuming a record.
 type ConsumeResponse struct {
 	Record Record `json:"record"`
 }
 
-// Create a new LogStore.
+// newLogStore creates a new LogStore instance.
 func newLogStore() *LogStore {
 	return &LogStore{
 		Log: NewLog(),
 	}
 }
 
+// handleProduce handles the HTTP POST request for producing a new record to the LogStore.
+// It reads the JSON payload from the request body, decodes it into a `ProduceRequest` struct,
+// appends the record to the LogStore's Log, retrieves the offset where it was appended,
+// and constructs a ProduceResponse containing the offset of the appended record and encodes it as JSON in the response body.
+//
+// It returns an error if decoding fails (Bad Request),
+// if the append operation encounters an error (Internal Server Error),
+// or if encoding the response fails (Internal Server Error).
 func (ls *LogStore) handleProduce(w http.ResponseWriter, r *http.Request) {
 	var req ProduceRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
@@ -58,10 +71,15 @@ func (ls *LogStore) handleProduce(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// handleConsume handles the HTTP GET request for consuming a record from the LogStore.
+// It reads the offset from the request parameters, retrieves the corresponding record from the LogStore's Log,
+// and constructs a ConsumeResponse containing the retrieved record.
+// Errors if the offset is not found (Not Found).
 func (ls *LogStore) handleConsume(w http.ResponseWriter, r *http.Request) {
-
+	// TODO: Implement handleConsume logic
 }
 
+// NewHTTPServer creates a new HTTP server with the specified URI and returns it.
 func NewHTTPServer(uri string) *http.Server {
 	logStore := newLogStore()
 	r := mux.NewRouter()
